bba78e60d467dbeca7c978ba577f531238c52513
Humna Javaid <humnajavaid142@gmail.com>
2026-01-04 10:03:42 +0500

    Initial commit : Hospital Patient Record System

---
+++ hospital_patient_record_system.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// patient record
+void addPatient() {
+    int id;
+    int age;
+    string name;
+    string disease;
+
+    cout << "\nEnter Patient ID: ";
+    cin >> id;
+    cin.ignore();
+
+    cout << "Enter Patient Name: ";
+    getline(cin, name);
+
+    cout << "Enter Patient Age: ";
+    cin >> age;
+    cin.ignore();
+
+    cout << "Enter Disease: ";
+    getline(cin, disease);
+
+    ofstream file("patients.csv", ios::app);
+
+    // patient informatiom
+    if (file.tellp() == 0) {
+        file << "ID,Name,Age,Disease\n";
+    }
+
+    file << id << "," << name << "," << age << "," << disease << "\n";
+    file.close();
+
+    cout << "\nPatient record saved successfully.\n";
+}
+
+//  all records (cvs file)
+void displayPatients() {
+    ifstream file("patients.csv");
+    string line;
+
+    if (!file) {
+        cout << "\nNo patient records found.\n";
+        return;
+    }
+
+    cout << "\n--- Patient Records ---\n";
+    while (getline(file, line)) {
+        cout << line << endl;
+    }
+
+    file.close();
+}
+
+// search patient by ID
+void searchPatient() {
+    ifstream file("patients.csv");
+    string line, searchID;
+    bool found = false;
+
+    if (!file) {
+        cout << "\nNo patient records found.\n";
+        return;
+    }
+
+    cout << "\nEnter Patient ID to search: ";
+    cin >> searchID;
+
+    // skip header line
+    getline(file, line);
+
+    while (getline(file, line)) {
+        string id = line.substr(0, line.find(','));
+
+        if (id == searchID) {
+            cout << "\nPatient Found:\n";
+            cout << "ID,Name,Age,Disease\n";
+            cout << line << endl;
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        cout << "\nPatient not found.\n";
+    }
+
+    file.close();
+}
+
+int main() {
+    int choice;
+
+    do {
+        cout << "\n Hospital Patient Record System";
+        cout << "\n1. Add Patient Record";
+        cout << "\n2. Display All Patients";
+        cout << "\n3. Search Patient by ID";
+        cout << "\n4. Exit";
+        cout << "\nEnter your choice: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            addPatient();
+        }
+        else if (choice == 2) {
+            displayPatients();
+        }
+        else if (choice == 3) {
+            searchPatient();
+        }
+        else if (choice == 4) {
+            cout << "\nExiting program...\n";
+        }
+        else {
+            cout << "\nInvalid choice. Try again.\n";
+        }
+
+    } while (choice != 4);
+
+    return 0;
+}
